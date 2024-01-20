1f2d238f4c7b29b9bd0bd4be2f9dfe3cb108df7e
Zachary Lee <w.zachary.lee@gmail.com>
2024-01-20 10:32:41 -0500

    fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,16 +30,16 @@ export default function Home() {
       <main>
         <h3 className="mb-6 text-2xl font-bold">Hi! I&apos;m Zack 👋</h3>
         <div className="mb-3">
-          Founder and CEO of{" "}
+          I&apos;m the founder of{" "}
           <Link
             className="text-indigo-500 underline"
             href="https://onplayground.com"
           >
             Playground Education
           </Link>
-          , an AI research lab reimagining consumer education in the age of
-          large language models. Was previously a Data Scientist / AI Engineer
-          at places like{" "}
+          , a research lab reimagining education in the age of generative AI.
+          Previously, I&apos;ve worked as as a Data Scientist / Developer /
+          Designer at places like{" "}
           <Link className="text-indigo-500 underline" href="https://copy.ai">
             CopyAI
           </Link>
@@ -53,79 +53,6 @@ export default function Home() {
           , and{" "}
           <Link className="text-indigo-500 underline" href="https://indeed.com">
             Indeed.
-          </Link>
-        </div>
-        <div className="mb-3">
-          I write about LLMs, startups, and education:
-          <ul className="list-inside list-disc">
-            <li>
-              <Link
-                className="text-indigo-500 underline"
-                href="/posts/function-calling-in-claude"
-              >
-                Experiment: Function calling with Claude 2
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-indigo-500 underline"
-                href="/posts/llms-and-frameworks"
-              >
-                An LLM is only as good as its framework
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="mb-3">
-          I make fun products that help people learn:
-          <ul className="list-inside list-disc">
-            <li>
-              <Link
-                className="text-indigo-500 underline"
-                href="https://withabstract.com"
-              >
-                <span className="font-semibold">Abstract</span> - daily
-                summaries of the hottest research papers
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-indigo-500 underline"
-                href="https://withartisan.com"
-              >
-                <span className="font-semibold">Artisan</span> - an AI-powered
-                personal trainer for self-guided learners
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-indigo-500 underline"
-                href="https://www.youtube.com/watch?v=ujOuXpvTss0"
-              >
-                <span className="font-semibold">My web dev bootcamp</span> - a
-                free modern web development bootcamp on Youtube
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-indigo-500 underline"
-                href="https://tacithq.com"
-              >
-                <span className="font-semibold">Tacit</span> - an AI-powered
-                copilot for desktop apps. Still in development.
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="mb-3">
-          I&apos;m also occasionally available for consulting work. You can
-          reach me at{" "}
-          <Link
-            className="text-indigo-500 underline"
-            href="mailto:hi@wenquai.com"
-          >
-            hi@wenquai.com
           </Link>
         </div>
         <div className="mb-3">
