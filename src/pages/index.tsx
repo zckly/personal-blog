210502c0a6a7a3ae65890d8e7f9ea96d3ccf7e1e
Zachary Lee <w.zachary.lee@gmail.com>
2024-01-22 15:38:34 -0500

    no more insta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -98,13 +98,6 @@ export default function Home() {
           ,{" "}
           <Link
             className="text-indigo-500 underline"
-            href="https://instagram.com/wenquai"
-          >
-            Instagram
-          </Link>
-          ,{" "}
-          <Link
-            className="text-indigo-500 underline"
             href="https://github.com/zckly"
           >
             Github
